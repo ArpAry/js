2ce5554456d169cb1ab8e63cd73eb6286143b5f4
Arpit Arya <87825317+ArpAry@users.noreply.github.com>
2021-12-01 23:22:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,89 @@
+////lecture 2
+
+/*
+ alert("test");
+*/
+/********** */
+////lecture 3
+/*********/
+/*
+console.log("checking the console command");
+*/
+
+
+//*********** */
+//////lecture 5
+//**************** */
+//// console.log(document.getElementById("blue_box").innerHTML);
+// document.getElementById("blue_box").innerHTML="<h1> arpit </h1>";
+// console.log(document.getElementById("blue_box").innerHTML);
+
+
+/////////////////
+//lecture 6
+////////////////
+// var user_name="Arpit";
+// console.log(document.getElementById("user_greeting_message").innerHTML = "Hello  "+user_name+" !");
+//************ */
+
+
+
+///////
+/*  lecture 7 */
+//////
+
+// var first_Name="john";
+// console.log(typeof first_Name);
+// var last_Name="doe";
+// var full_Name=first_Name+"123"+last_Name;
+// console.log(full_Name);
+// console.log(typeof full_Name);
+// console.log(full_Name.length);
+// var url="https://www.udemy.com";
+// console.log(url.replace("https://" ,""));
+
+
+///////
+/*  lecture 8 */
+//////
+// var power=Math.pow(2,3);
+// console.log(power);
+// var roundedNumber=Math.round(2.73);
+
+// console.log(roundedNumber);
+//     var roundUp=Math.ceil(4.3);
+//     console.log(roundUp);
+// var roundDown=Math.floor(3.45);
+// console.log(roundDown);
+// var squareRoot=Math.sqrt(4);
+// console.log(squareRoot);
+// var randomNumber=Math.random()*100;
+// console.log(randomNumber);
+///////
+/*  lecture 9 */
+//////
+
+// var myNumber=3.75236;
+// console.log(myNumber.toFixed(2));
+// console.log(myNumber.toFixed(4));
+
+// var a=2;
+// a+=2;
+// console.log(a);
+
+// var c="arpit";
+// c+="arya";
+// console.log(c);
+
+
+// var firstNumber="21";
+// var secondNumber="4";
+// console.log(parseInt(firstNumber)+parseInt(secondNumber));
+
+
+// var dob=2002;
+// console.log(dob.toString()+"  is the year i born");
+
+///////
+/*  lecture 10 */
+//////
